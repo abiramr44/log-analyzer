1fa6d5db3a1b4cbedb191f6dd7371e42d8f1c3ce
Abiram <abiramr44@gmail.com>
2026-05-11 05:11:12 -0400

    Security: move email credentials to environment variables

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 EMAIL_ENABLED = False
-EMAIL_SENDER = "your@gmail.com"
-EMAIL_RECEIVER = "your@gmail.com"
-EMAIL_PASSWORD = ""
-EMAIL_SMTP = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_SENDER = os.environ.get('EMAIL_SENDER', '')
+EMAIL_RECEIVER = os.environ.get('EMAIL_RECEIVER', '')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', '')
+EMAIL_SMTP = os.environ.get('EMAIL_SMTP', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
